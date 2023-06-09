df66f69a27fe1746804e75a5feced079261e60dc
Lucas Santana <ln.santana08@gmail.com>
2023-06-09 20:53:06 -0300

    feat: shared config

---
+++ packages/eslint-config/node.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'quote-props': ['error', 'consistent'],
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+};
